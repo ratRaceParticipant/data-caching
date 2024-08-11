172edf0675dc233c2acf7aa0152a11645e6278a2
ratRaceParticipant <114292201+ratRaceParticipant@users.noreply.github.com>
2024-08-11 18:54:48 +0530

    Initial Commit

---
+++ DataCaching/DataCachingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataCachingApp.swift
+//  DataCaching
+//
+//  Created by Himanshu Karamchandani on 11/08/24.
+//
+
+import SwiftUI
+
+@main
+struct DataCachingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
